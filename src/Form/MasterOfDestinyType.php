dfc3ffaea2c5db1dcaff6b9353c6ad8bed7951fe
GRONDIN Clarel* <clarelgrondin@gmail.com>
2024-11-28 11:24:43 +0400

    reglage bug logout, correction front, correction crud mod

--- src/Form/MasterOfDestinyType.php
+++ src/Form/MasterOfDestinyType.php
@@ -6,6 +6,7 @@ use App\Entity\MasterOfDestiny;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class MasterOfDestinyType extends AbstractType
@@ -13,9 +14,36 @@ class MasterOfDestinyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('pseudo')
-            ->add('password', PasswordType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom: ',
+                'label_attr' => [
+                    'class' => 'block mb-2 text-indigo-500', 
+                ],
+                'attr' => [
+                    'class' => 'w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300', 
+                    'placeholder' => 'Entrez le nom du maître',
+                ],
+            ])
+            ->add('pseudo', TextType::class, [
+                'label' => 'Pseudo: ',
+                'label_attr' => [
+                    'class' => 'block mb-2 text-indigo-500', 
+                ],
+                'attr' => [
+                    'class' => 'w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300', 
+                    'placeholder' => 'Entrez le pseudo de connexion',
+                ],
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe: ',
+                'label_attr' => [
+                    'class' => 'block mb-2 text-indigo-500', 
+                ],
+                'attr' => [
+                    'class' => 'w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300', 
+                    'placeholder' => 'Entrez le mot de passe',
+                ],
+            ])
         ;
     }
 
